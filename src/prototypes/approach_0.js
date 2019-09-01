cdb768669a421cc04cb30ce350067e750ff6621d
Suraj Verma <suraj.verma@bbc.co.uk>
2019-09-01 11:58:38 +0100

    update prototypes

---
+++ src/prototypes/approach_0.js
@@ -0,0 +1,33 @@
+const add = function (num1, num2) {
+  return num1 + num2
+}
+
+const subtract = function (num1, num2) {
+  return num1 - num2
+}
+
+const multiply = function (num1, num2) {
+  return num1 * num2
+}
+
+const divide = function (num1, num2) {
+  return num1 / num2
+}
+
+const modulus = function (num1, num2) {
+  return num1 % num2
+}
+
+console.log('USER 1 RESULTS')
+console.log(add(20, 4))
+console.log(subtract(20, 4))
+console.log(multiply(20, 4))
+console.log(divide(20, 4))
+console.log(modulus(20, 4))
+
+console.log('USER 2 RESULTS')
+console.log(add(50, 10))
+console.log(subtract(50, 10))
+console.log(multiply(50, 10))
+console.log(divide(50, 10))
+console.log(modulus(50, 10))
