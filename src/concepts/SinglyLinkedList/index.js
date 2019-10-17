aaf03acf0226cfcb752702a9cfe61677035fdbaf
Suraj Verma <suraj.verma@bbc.co.uk>
2019-10-17 08:37:23 +0100

    update methods on SinglyLinkedList class

--- src/concepts/SinglyLinkedList/index.js
+++ src/concepts/SinglyLinkedList/index.js
@@ -1,7 +1,7 @@
 class LinkedList {
-  constructor(value) {
-    if (value) {
-      const node = { value, next: null }
+  constructor(data) {
+    if (data) {
+      const node = { data, next: null }
       this._head = node
     } else {
       this._head = {}
@@ -9,14 +9,39 @@ class LinkedList {
     this._tail = this._head
   }
 
-  insert(value) {
-    const node = { value, next: null }
+  insert(data) {
+    const node = { data, next: null }
     if (this.isEmpty()) {
       this._head = node
       this._tail = this._head
     } else {
       this._tail.next = node
       this._tail = node
+    }
+  }
+
+  contains(data) {
+    let node = this._head
+    while (node && node.data !== data) {
+      node = node.next
+    }
+
+    return (node && node.data) === data
+  }
+
+  removeNode(data) {
+    let node = this._head
+    let previousNode = this._head
+    while (node.data !== data) {
+      previousNode = node
+      node = node.next
+    }
+    if (node === this._head) {
+      this.removeHead()
+    } else if (node === this._tail) {
+      this.removeTail()
+    } else {
+      previousNode.next = node.next
     }
   }
 
--- src/concepts/SinglyLinkedList/index.test.js
+++ src/concepts/SinglyLinkedList/index.test.js
@@ -5,14 +5,14 @@ test('should return the empty LinkedList constructor object', () => {
 })
 
 test('should insert the first item via the constructor', () => {
-  expect(new LinkedList(1)).toEqual({ _head: { value: 1, next: null }, _tail: { value: 1, next: null } })
+  expect(new LinkedList(1)).toEqual({ _head: { data: 1, next: null }, _tail: { data: 1, next: null } })
 })
 
 test('should insert the first item via the insert method', () => {
   const myLinkedList = new LinkedList()
   myLinkedList.insert(1)
 
-  expect(myLinkedList).toEqual({ _head: { value: 1, next: null }, _tail: { value: 1, next: null } })
+  expect(myLinkedList).toEqual({ _head: { data: 1, next: null }, _tail: { data: 1, next: null } })
 })
 
 test('should insert 2 items via the insert method', () => {
@@ -20,14 +20,14 @@ test('should insert 2 items via the insert method', () => {
   myLinkedList.insert(1)
   myLinkedList.insert(2)
 
-  expect(myLinkedList).toEqual({ _head: { value: 1, next: { value: 2, next: null } }, _tail: { value: 2, next: null } })
+  expect(myLinkedList).toEqual({ _head: { data: 1, next: { data: 2, next: null } }, _tail: { data: 2, next: null } })
 })
 
 test('should insert 2 items via the constructor and insert method', () => {
   const myLinkedList = new LinkedList(1)
   myLinkedList.insert(2)
 
-  expect(myLinkedList).toEqual({ _head: { value: 1, next: { value: 2, next: null } }, _tail: { value: 2, next: null } })
+  expect(myLinkedList).toEqual({ _head: { data: 1, next: { data: 2, next: null } }, _tail: { data: 2, next: null } })
 })
 
 test('should insert 3 items via the constructor and insert method', () => {
@@ -35,7 +35,7 @@ test('should insert 3 items via the constructor and insert method', () => {
   myLinkedList.insert(2)
   myLinkedList.insert(3)
 
-  expect(myLinkedList).toEqual({ _head: { value: 1, next: { value: 2, next: { value: 3, next: null } } }, _tail: { value: 3, next: null } })
+  expect(myLinkedList).toEqual({ _head: { data: 1, next: { data: 2, next: { data: 3, next: null } } }, _tail: { data: 3, next: null } })
 })
 
 test('should remove the tail from a linked list with 1 item', () => {
@@ -49,7 +49,7 @@ test('should remove the tail from a linked list with 2 items', () => {
   myLinkedList.insert(2)
   myLinkedList.removeTail()
 
-  expect(myLinkedList).toEqual({ _head: { value: 1, next: null }, _tail: { value: 1, next: null } })
+  expect(myLinkedList).toEqual({ _head: { data: 1, next: null }, _tail: { data: 1, next: null } })
 })
 
 test('should remove the tail from a linked list with 3 items', () => {
@@ -58,7 +58,7 @@ test('should remove the tail from a linked list with 3 items', () => {
   myLinkedList.insert(3)
   myLinkedList.removeTail()
 
-  expect(myLinkedList).toEqual({ _head: { value: 1, next: { value: 2, next: null } }, _tail: { value: 2, next: null } })
+  expect(myLinkedList).toEqual({ _head: { data: 1, next: { data: 2, next: null } }, _tail: { data: 2, next: null } })
 })
 
 test('should not remove the tail from an empty linked list', () => {
@@ -79,7 +79,7 @@ test('should remove the head from a linked list with 2 items', () => {
   myLinkedList.insert(2)
   myLinkedList.removeHead()
 
-  expect(myLinkedList).toEqual({ _head: { value: 2, next: null }, _tail: { value: 2, next: null } })
+  expect(myLinkedList).toEqual({ _head: { data: 2, next: null }, _tail: { data: 2, next: null } })
 })
 
 test('should remove the head from a linked list with 3 items', () => {
@@ -88,7 +88,7 @@ test('should remove the head from a linked list with 3 items', () => {
   myLinkedList.insert(3)
   myLinkedList.removeHead()
 
-  expect(myLinkedList).toEqual({ _head: { value: 2, next: { value: 3, next: null } }, _tail: { value: 3, next: null } })
+  expect(myLinkedList).toEqual({ _head: { data: 2, next: { data: 3, next: null } }, _tail: { data: 3, next: null } })
 })
 
 test('should not remove the head from an empty linked list', () => {
@@ -120,7 +120,7 @@ test('should return FALSE when linked list has no items', () => {
 test('should reset a linked list', () => {
   const myLinkedList = new LinkedList(1)
   myLinkedList.insert(2)
-  expect(myLinkedList).toEqual({ _head: { value: 1, next: { value: 2, next: null } }, _tail: { value: 2, next: null } })
+  expect(myLinkedList).toEqual({ _head: { data: 1, next: { data: 2, next: null } }, _tail: { data: 2, next: null } })
   myLinkedList.reset()
   expect(myLinkedList).toEqual({ _head: {}, _tail: {} })
 })
@@ -128,15 +128,47 @@ test('should reset a linked list', () => {
 test('should return the head property', () => {
   const myLinkedList = new LinkedList(1)
   myLinkedList.insert(2)
-  expect(myLinkedList.head).toEqual({ value: 1, next: { value: 2, next: null }})
+  expect(myLinkedList.head).toEqual({ data: 1, next: { data: 2, next: null }})
 })
 
 test('should return the tail property', () => {
   const myLinkedList = new LinkedList(1)
   myLinkedList.insert(2)
-  expect(myLinkedList.tail).toEqual({ value: 2, next: null })
+  expect(myLinkedList.tail).toEqual({ data: 2, next: null })
 })
 
+test('should return TRUE if node contains the data', () => {
+  const myLinkedList = new LinkedList(1)
+  myLinkedList.insert(2)
+  expect(myLinkedList.contains(2)).toBe(true)
+})
 
+test('should return FALSE if node does not contains the data', () => {
+  const myLinkedList = new LinkedList(1)
+  myLinkedList.insert(2)
+  expect(myLinkedList.contains(3)).toBe(false)
+})
 
+test('should remove node by value from the middle of a linked list', () => {
+  const myLinkedList = new LinkedList(1)
+  myLinkedList.insert(2)
+  myLinkedList.insert(3)
+  myLinkedList.removeNode(2)
+  expect(myLinkedList).toEqual({_head: { data: 1, next: { data: 3, next: null } }, _tail: { data: 3, next: null } })
+})
 
+test('should remove node by value from the head of a linked list', () => {
+  const myLinkedList = new LinkedList(1)
+  myLinkedList.insert(2)
+  myLinkedList.insert(3)
+  myLinkedList.removeNode(1)
+  expect(myLinkedList).toEqual({_head: { data: 2, next: { data: 3, next: null } }, _tail: { data: 3, next: null } })
+})
+
+test('should remove node by value from the tail of a linked list', () => {
+  const myLinkedList = new LinkedList(1)
+  myLinkedList.insert(2)
+  myLinkedList.insert(3)
+  myLinkedList.removeNode(3)
+  expect(myLinkedList).toEqual({_head: { data: 1, next: { data: 2, next: null } }, _tail: { data: 2, next: null } })
+})
