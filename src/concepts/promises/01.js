31db02487d37bc9acd4dadbadade0f1e86b9530e
Suraj Verma <suraj.verma@booking.com>
2020-06-05 11:44:59 +0100

    update promises

--- src/concepts/promises/01.js
+++ src/concepts/promises/01.js
@@ -1,34 +1,9 @@
-const executor = (resolve, reject) => {
-  const onTimeoutComplete = () => {
-    // Change this to true to trigger the reject function
-    const hasError = false;
-
-    if (hasError) {
-      reject("timer threw an error");
-    }
-
-    resolve("timer value");
-  };
-
-  setTimeout(onTimeoutComplete, 1000);
+const displayData = (response) => {
+  console.log(response);
 };
 
-const myPromise = new Promise(executor);
+const getDataFromServer = fetch("https://some-endpoint.com");
 
-const firstThenFunc = function (value) {
-  console.log(`First then receives ${value}`);
-  return "first then value";
-};
+getDataFromServer.then(displayData);
 
-const secondThenFunc = function (value) {
-  console.log(`Second then receives ${value}`);
-  return "second then value";
-};
-
-const errorHandler = (err) => {
-  console.log(err);
-};
-
-myPromise.then(firstThenFunc).then(secondThenFunc).catch(errorHandler);
-
-console.log(myPromise);
+console.log("End of app");
